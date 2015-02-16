31dd2092cb2f5db4f45abad5916005f2950351cd
Brian Capouch <brianc@saintjoe.edu>
2015-02-16 02:45:41 -0500

    Clean up indentation for exam

--- baseConvert.js
+++ baseConvert.js
@@ -3,15 +3,16 @@ load('stack.js');
 function mulBase(num, base) {
  var s = new Stack();
  do {
- s.push(num % base);
- num = Math.floor(num /= base);
- } while (num > 0);
-var converted = "";
+   s.push(num % base);
+   num = Math.floor(num /= base);
+   } while (num > 0);
+
+ var converted = "";
  while (s.length() > 0) {
- converted += s.pop();
- }
+   converted += s.pop();
+   }
  return converted;
-}
+ }
 
 var num = 32;
 var base = 2;
