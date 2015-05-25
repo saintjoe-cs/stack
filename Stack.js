a5a2026e30942bc1a779a2695ba19a4e7dbd3aa9
Brian Capouch <brianc@proto.palaver.net>
2015-05-25 02:01:36 -0400

    Further sync with book

--- Stack.js
+++ Stack.js
@@ -1,31 +1,31 @@
 function Stack() {
- this.dataStore = [];
- this.top = 0;
- this.push = push;
- this.pop = pop;
- this.peek = peek;
- this.length = length;
- this.clear = clear;
+  this.dataStore = [];
+  this.top = 0;
+  this.push = push;
+  this.pop = pop;
+  this.peek = peek;
+  this clear = clear;
+  this.length = length;
 }
 
 function push(element) {
- this.dataStore[this.top++] = element;
+  this.dataStore[this.top++] = element;
 }
 
 function pop() {
- return this.dataStore[--this.top];
+  return this.dataStore[--this.top];
 }
 
 function peek() {
- return this.dataStore[this.top-1];
+  return this.dataStore[this.top-1];
 }
 
 function length() {
- return this.top;
+  return this.top;
 }
 
 function clear() {
- this.top = 0;
- this.dataStore.length = 0;
+  this.top = 0;
+  this.dataStore.length = 0;
 }
 
