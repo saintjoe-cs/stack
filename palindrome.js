64b482e2b67640618b835b6646824f9318e50a2a
Brian Capouch <brianc@proto.palaver.net>
2015-05-25 02:09:08 -0400

    Fix up and ready to move name

--- palindrome.js
+++ palindrome.js
@@ -1,33 +1,33 @@
-load ("stack.js");
+load ("Stack.js");
+
 function isPalindrome(word) {
- var s = new Stack();
- for (var i = 0; i < word.length; ++i) {
- s.push(word[i]);
- }
- var rword = "";
- while (s.length() > 0) {
- rword += s.pop();
- }
- if (word == rword) {
- return true;
- }
- else {
- return false;
- }
+  var s = new Stack();
+  for (var i = 0; i < word.length; ++i) {
+    s.push(word[i]);
+  }
+  var rword = "";
+  while (s.length() > 0) {
+    rword += s.pop();
+  }
+  if (word == rword) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-// Test it
 var word = "hello";
 if (isPalindrome(word)) {
- print(word + " is a palindrome.");
+  print(word + " is a palindrome.");
 }
 else {
- print(word + " is not a palindrome.");
+  print(word + " is not a palindrome.");
 }
 word = "racecar"
 if (isPalindrome(word)) {
- print(word + " is a palindrome.");
+  print(word + " is a palindrome.");
 }
 else {
- print(word + " is not a palindrome.");
+  print(word + " is not a palindrome.");
 }
